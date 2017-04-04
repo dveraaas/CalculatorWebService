9c01ff5f940e73860dc6dff2796fcdf127469446
diego.vera <diego.vera@evicertia.com>
2017-04-04 13:20:31 +0200

    Application ASP.NET MVC C# with Logs

---
+++ CalculatorService.server/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CalculatorService.server.Startup))]
+namespace CalculatorService.server
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
