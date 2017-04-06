724c471f2b11ab07b8e014a655f6e3f8cab92b09
diego.vera <diego.vera@evicertia.com>
2017-04-06 15:57:43 +0200

    Remove unnecesary files on server

---
+++ CalculatorWebService/CalculatorService.server/Models/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorService.server.Models
+{
+	public class Response
+	{
+		public string operation { get; set; }
+		public int Total { get; set; }
+	}
+}
---
+++ CalculatorWebService/CalculatorService.server/Startup.cs
@@ -0,0 +1,16 @@
+﻿#if true
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CalculatorService.server.Startup))]
+namespace CalculatorService.server
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+           // ConfigureAuth(app);
+        }
+    }
+}
+#endif
